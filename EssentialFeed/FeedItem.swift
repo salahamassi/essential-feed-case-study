32ffd025579d9dfe09c09163631213d4d3a1ade5
salah nahed <salahnahed@icloud.com>
2021-02-06 02:36:14 +0200

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Salah Amassi on 06/02/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
